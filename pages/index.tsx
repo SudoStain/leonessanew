b01880b185b973b6c856fb4f7705b7196e1ddc2d
Steyn Kalala <steynmkalala@gmail.com>
2021-11-22 13:27:49 -0500

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,10 @@ export default function Home() {
         <div className="h-1 mx-auto bg-white w-1/4  my-0 py-0 rounded-t">
 
         {!user ? <LoginForm /> : <Todo />}
+
+        <div>
         {!user ? <LoginForm /> : <Homer/>}
+        <div/>
         </div>
       </div>
 
